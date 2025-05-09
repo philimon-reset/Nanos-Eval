cc52c3123e4b23a081f608bd6eb4534541e83750
Philimon Derib Ayalew <philimonset@gmail.com>
2025-05-09 16:59:57 +0200

    new plots

--- Network-Based/Go/plot_usage.py
+++ Network-Based/Go/plot_usage.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+plt.rcParams.update({'font.size': 17})
+
 
 def convert_latency(val):
     if isinstance(val, str):
@@ -12,49 +14,47 @@ def convert_latency(val):
     return pd.NA
 
 
-def plot_metric(df1, df2, x_col, y_col, labels, title, ylabel, plot_path):
+def plot_metric(ax, df1, df2, x_col, y_col, labels, fig_title, ylabel):
     """Reusable function to plot a metric."""
-    plt.rcParams.update({'font.size': 18})
-    plt.figure(figsize=(12, 6))
-    plt.plot(df1[x_col], df1[y_col], label=labels[0], marker='o')
-    plt.plot(df2[x_col], df2[y_col], label=labels[1], marker='x')
-    plt.ylim(0, None)  # Set y-axis limit to start from 0
-    plt.title(title)
-    plt.xlabel("Time Elapsed (s)")
-    plt.ylabel(ylabel)
-    plt.legend()
-    plt.grid(True)
-
-    plt.savefig(plot_path)
-    plt.close()
-    print(f"📈 Plot saved to {plot_path}")
-
+    ax.plot(df1[x_col], df1[y_col], label=labels[0], marker='o')
+    ax.plot(df2[x_col], df2[y_col], label=labels[1], marker='x')
+    ax.set_ylim(0, None)  # Set y-axis limit to start from 0
+    ax.legend()
+    ax.grid(True)
+    ax.set_title(fig_title)
+    ax.set_xlabel("Time Elapsed (s)")
+    ax.set_ylabel(ylabel)
 
 def compare_resource_usage_plot(first_process, second_process):
     first_df = pd.read_csv(first_process[0])
     second_df = pd.read_csv(second_process[0])
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
 
-    cpu_plot_path = f"metrics/plots/comparitive/{first_process[1]}_vs_{second_process[1]}_cpu_usage.png"
+    # Plot CPU usage
+    plot_path = f"metrics/plots/comparitive/{first_process[1]}_vs_{second_process[1]}_resource_usage.png"
     plot_metric(
+        ax1,
         first_df, second_df,
         x_col="Time Elapsed", y_col="CPU%",
         labels=[
             f"{first_process[1]} CPU Usage (%)", f"{second_process[1]} CPU Usage (%)"],
-        title="Comparative CPU Usage Over Time",
-        ylabel="CPU Usage (%)",
-        plot_path=cpu_plot_path
+        fig_title="Comparative CPU Usage Over Time",
+        ylabel="CPU Usage (%)"
     )
 
-    memory_plot_path = f"metrics/plots/comparitive/{first_process[1]}_vs_{second_process[1]}_memory_usage.png"
+    # Plot Memory usage
     plot_metric(
+        ax2,
         first_df, second_df,
         x_col="Time Elapsed", y_col="Memory(MB)",
         labels=[
             f"{first_process[1]} Memory Usage (MB)", f"{second_process[1]} Memory Usage (MB)"],
-        title="Comparative Memory Usage Over Time",
-        ylabel="Memory Usage (MB)",
-        plot_path=memory_plot_path
+        fig_title="Comparative Memory Usage Over Time",
+        ylabel="Memory Usage (MB)"
     )
+    fig.tight_layout()
+    fig.savefig(plot_path)
+    print(f"📈 Plot saved to {plot_path}")
 
 
 def plot_webserver_metric(nanos_df, docker_df, field_pair):
@@ -70,26 +70,23 @@ def plot_webserver_metric(nanos_df, docker_df, field_pair):
     # Prepare data for nanos and docker
     nanos_data = prepare_data(nanos_df)
     docker_data = prepare_data(docker_df)
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 8), gridspec_kw={'hspace': 0.5})
 
-    nanos_min_max_path = f"metrics/plots/webserver/min_max/nanos_{field_pair[0]}.png"
-    nanos_box_plot_path = f"metrics/plots/webserver/box_plot/nanos_{field_pair[0]}.png"
+    box_plot_path = f"metrics/plots/webserver/box_plot/nanos_and_docker_{field_pair[0]}.png"
+    ax1.set_title(
+        f"Distribution of {field_pair[1]} for Nanos")
+    ax2.set_title(
+        f"Distribution of {field_pair[1]} for Docker")
+    plot_box_plot(ax1, nanos_df, field_pair)
+    plot_box_plot(ax2, docker_df, field_pair)
+    fig.savefig(box_plot_path)
 
-    # plot_min_mean_max_graph(nanos_data, field_pair[1],  nanos_min_max_path)
-    plot_box_plot(nanos_df, field_pair, nanos_box_plot_path)
-
-    docker_min_max_path = f"metrics/plots/webserver/min_max/docker_{field_pair[0]}.png"
-    docker_box_plot_path = f"metrics/plots/webserver/box_plot/docker_{field_pair[0]}.png"
-
-    # plot_min_mean_max_graph(docker_data, field_pair[1], docker_min_max_path)
-    plot_box_plot(docker_df, field_pair, docker_box_plot_path)
 
     comparison_path = f"metrics/plots/comparitive/webserver/bar_plot/docker_vs_nanos_{field_pair[0]}.png"
     plot_bar_with_error(nanos_data, docker_data, field_pair, comparison_path)
 
 
-def plot_box_plot(data, field_pair, path):
-    plt.rcParams.update({'font.size': 16})
-    fig, ax = plt.subplots(figsize=(10, 6))
+def plot_box_plot(ax, data, field_pair):
     # Prepare data: group values by connection count
     connection_levels = [10, 50, 100]
     data = [data[data["connection"] == level][field_pair[0]].dropna().tolist()
@@ -101,41 +98,37 @@ def plot_box_plot(data, field_pair, path):
         patch.set_facecolor(color)
     ax.set_xlabel("Concurrent Connections")
     ax.set_ylabel(field_pair[1])
-    ax.set_title(f"Distribution of {field_pair[1]} by Connection Count")
-
-    plt.savefig(path)
-    plt.close()
 
 
-def plot_min_mean_max_graph(data, ylabel, path):
-    x = [10, 50, 100]
-    y = [float(data[str(val)]['mean']) for val in x]
-
-    fig, ax = plt.subplots()
-    ax.set_xticks(x)
-    ax.set_xlabel("Concurrent Connections")
-    ax.set_ylabel(ylabel)
-    ax.set_title(f"Min, Mean, Max Graph of {ylabel}.")
-    error_below = [float(data[str(val)]['mean']) -
-                   float(data[str(val)]['min']) for val in x]
-    error_above = [float(data[str(val)]['max']) -
-                   float(data[str(val)]['mean']) for val in x]
-    error = [error_below, error_above]
-
-    ax.errorbar(
-        x,
-        y,
-        yerr=error,
-        capsize=5,
-        ecolor="lightblue",
-        markerfacecolor="black",
-        markeredgecolor="black",
-        marker="o",
-        linestyle="none",
-    )
-
-    plt.savefig(path)
-    plt.close()
+# def plot_min_mean_max_graph(data, ylabel, path):
+#     x = [10, 50, 100]
+#     y = [float(data[str(val)]['mean']) for val in x]
+#
+#     fig, ax = plt.subplots()
+#     ax.set_xticks(x)
+#     ax.set_xlabel("Concurrent Connections")
+#     ax.set_ylabel(ylabel)
+#     ax.set_title(f"Min, Mean, Max Graph of {ylabel}.")
+#     error_below = [float(data[str(val)]['mean']) -
+#                    float(data[str(val)]['min']) for val in x]
+#     error_above = [float(data[str(val)]['max']) -
+#                    float(data[str(val)]['mean']) for val in x]
+#     error = [error_below, error_above]
+#
+#     ax.errorbar(
+#         x,
+#         y,
+#         yerr=error,
+#         capsize=5,
+#         ecolor="lightblue",
+#         markerfacecolor="black",
+#         markeredgecolor="black",
+#         marker="o",
+#         linestyle="none",
+#     )
+#
+#     plt.savefig(path)
+#     plt.close()
 
 # Plotting function
 
